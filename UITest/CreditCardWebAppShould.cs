4de4749e94bb70e738d14be476412a6ca2384061
L. M <a.f.girl89@hotmail.com>
2020-11-29 13:39:05 +0200

    Add project files.

---
+++ UITest/CreditCardWebAppShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UITest
+{
+  public  class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("Category","Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://passport-photo.online/pl/zdjecie-do-mlegitymacji");
+                string pageTitle = driver.Title;
+                Assert.Equal("Zdjęcie do mLegitymacji - Passport-Photo.online", pageTitle);
+            }
+        }
+    }
+
+}
