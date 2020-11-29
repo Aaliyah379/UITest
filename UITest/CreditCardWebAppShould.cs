ef406bec1ee23fa39b3138932fd854442a33f707
L. M <a.f.girl89@hotmail.com>
2020-11-29 23:17:27 +0200

    Added 3 test to check if the  photo can be uploaded

--- UITest/CreditCardWebAppShould.cs
+++ UITest/CreditCardWebAppShould.cs
@@ -8,23 +8,21 @@ namespace UITest
     public class CreditCardWebAppShould
 
     {
-        private const string homeUrl = "https://passport-photo.online/pl/zdjecie-do-mlegitymacji";
-        private const string homeTitle = "Zdjęcie do mLegitymacji - Passport-Photo.online";
-
+        private const string homeUrl = "https://passport-photo.online/en-gb";
+        private const string AboutUrl = "https://passport-photo.online/en-gb/contact/";
+        private const string homeTitle = "Passport and Visa photos online - Passport-Photo.online";
 
         [Fact]
         [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
         {
-          
             using (IWebDriver driver = new ChromeDriver())
             {
-                
-                driver.Navigate().GoToUrl(homeUrl);
 
+                driver.Navigate().GoToUrl(homeUrl);
                 DemoHelper.Pause();
 
-                Assert.Equal( homeTitle,driver.Title);
+                Assert.Equal(homeTitle, driver.Title);
                 Assert.Equal(homeUrl, driver.Url);
             }
         }
@@ -34,7 +32,7 @@ namespace UITest
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                
+
                 driver.Navigate().GoToUrl(homeUrl);
 
                 DemoHelper.Pause();
@@ -42,10 +40,32 @@ namespace UITest
 
                 Assert.Equal(homeTitle, driver.Title);
                 Assert.Equal(homeUrl, driver.Url);
-
-
             }
+        }
 
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnBack()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //driver.Navigate().GoToUrl(homeUrl);
+                //DemoHelper.Pause();
+                //driver.Navigate().GoToUrl(AboutUrl);
+                //DemoHelper.Pause();
+                //driver.Navigate().Back();
+                //DemoHelper.Pause();
+
+                //Assert.Equal(homeTitle, driver.Title);
+                //Assert.Equal(homeUrl, driver.Url);
+
+                driver.Navigate().GoToUrl("https://passport-photo.online/en-gb/uk-linkedin-photo");
+                var addFile = driver.FindElement(By.Id("upload-form-file-input"));
+                addFile.SendKeys(@"C:\Users\robby\Desktop\Game of Thrones\228408_600.jpg");
+
+
+                //TODO: assert that page was reloaded
+            }
         }
     }
 }
