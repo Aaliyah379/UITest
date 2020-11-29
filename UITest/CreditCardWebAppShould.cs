a76df740c57d487d80a2e41a48c75a0af032230f
L. M <a.f.girl89@hotmail.com>
2020-11-29 14:55:05 +0200

    Added second test to reload the browser  using  Refresh method

--- UITest/CreditCardWebAppShould.cs
+++ UITest/CreditCardWebAppShould.cs
@@ -5,19 +5,47 @@ using OpenQA.Selenium.Chrome;
 
 namespace UITest
 {
-  public  class CreditCardWebAppShould
+    public class CreditCardWebAppShould
+
     {
+        private const string homeUrl = "https://passport-photo.online/pl/zdjecie-do-mlegitymacji";
+        private const string homeTitle = "Zdjęcie do mLegitymacji - Passport-Photo.online";
+
+
         [Fact]
-        [Trait("Category","Smoke")]
+        [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
+        {
+          
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                
+                driver.Navigate().GoToUrl(homeUrl);
+
+                DemoHelper.Pause();
+
+                Assert.Equal( homeTitle,driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void reloadPage()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("https://passport-photo.online/pl/zdjecie-do-mlegitymacji");
-                string pageTitle = driver.Title;
-                Assert.Equal("Zdjęcie do mLegitymacji - Passport-Photo.online", pageTitle);
+                
+                driver.Navigate().GoToUrl(homeUrl);
+
+                DemoHelper.Pause();
+                driver.Navigate().Refresh();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+
+
             }
+
         }
     }
-
 }
---
+++ UITest/DemoHelper.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Threading;
+
+
+namespace UITest
+{
+    internal static class DemoHelper
+    {
+        /////<summary>
+        ///Brief delay to slow down browser interaction for the test
+        /////</summary>
+        public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+    }
+}
+
