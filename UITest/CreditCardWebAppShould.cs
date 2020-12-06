30fb51097edbdb1ad5562b816d79b9578e623f74
Yurii.Moroziuk <Yuriy.Morozyuk.95@gmail.com>
2020-12-06 14:37:08 +0200

    add new tests

--- UITest/CreditCardWebAppShould.cs
+++ UITest/CreditCardWebAppShould.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace UITest
 {
-    public class CreditCardWebAppShould
+    using System;
+    using System.Diagnostics;
+    using OpenQA.Selenium.Remote;
+    using OpenQA.Selenium.Support.Events;
+    using Xunit.Abstractions;
 
+    public class CreditCardWebAppShould
     {
+        private readonly ITestOutputHelper _output;
         private const string homeUrl = "https://passport-photo.online/en-gb";
         private const string AboutUrl = "https://passport-photo.online/en-gb/contact/";
         private const string homeTitle = "Passport and Visa photos online - Passport-Photo.online";
 
+        private const string YURII_PHOTO = @"C:\Users\Yurii\Pictures\test\Julia-Roberts-Natural-Hair-Color.jpg";
+        private const string AALIYAH_PHOTO = @"C:\Users\robby\Desktop\Game of Thrones\228408_600.jpg";
+
+        private int navigationCount = 0; 
+
+        public CreditCardWebAppShould(ITestOutputHelper output)
+        {
+            _output = output;
+        }
         [Fact]
         [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
@@ -49,21 +63,15 @@ namespace UITest
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                //driver.Navigate().GoToUrl(homeUrl);
-                //DemoHelper.Pause();
-                //driver.Navigate().GoToUrl(AboutUrl);
-                //DemoHelper.Pause();
-                //driver.Navigate().Back();
-                //DemoHelper.Pause();
+                driver.Navigate().GoToUrl(homeUrl);
+                DemoHelper.Pause();
+                driver.Navigate().GoToUrl(AboutUrl);
+                DemoHelper.Pause();
+                driver.Navigate().Back();
+                DemoHelper.Pause();
 
-                //Assert.Equal(homeTitle, driver.Title);
-                //Assert.Equal(homeUrl, driver.Url);
-
-                driver.Navigate().GoToUrl("https://passport-photo.online/pl/zdjecie-do-mlegitymacji");
-                var addFile = driver.FindElement(By.Id("upload-form-file-input"));
-                addFile.SendKeys(@"C:\Users\robby\Desktop\Game of Thrones\228408_600.jpg");
-
-
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
                 //TODO: assert that page was reloaded
             }
         }
@@ -87,6 +95,51 @@ namespace UITest
 
                 //TODO: assert that page was reloaded
             }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void UserShouldBeAbleToUploadPhoto()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://passport-photo.online/pl/zdjecie-do-mlegitymacji");
+                var addFile = driver.FindElement(By.Id("upload-form-file-input"));
+
+                Assert.NotNull(addFile);
+
+                addFile.SendKeys(YURII_PHOTO);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ShowLoadingTimeAndNavigationCountWhileUploadingImage()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                var firingDriver = new EventFiringWebDriver(driver);
+                navigationCount = 0;
+
+                firingDriver.Navigating += FiringDriver_Navigated;
+
+                firingDriver.Navigate().GoToUrl("https://passport-photo.online/pl/zdjecie-do-mlegitymacji");
+                var addFile = firingDriver.FindElement(By.Id("upload-form-file-input"));
+
+                stopWatch.Start();
+                addFile.SendKeys(YURII_PHOTO);
+                stopWatch.Stop();
+            }
+
+            _output.WriteLine(stopWatch.Elapsed.ToString());
+            _output.WriteLine($"Navigation was called {navigationCount} times");
+        }
+
+        private void FiringDriver_Navigated(object sender, WebDriverNavigationEventArgs e)
+        {
+            navigationCount++;
         }
     }
 }
